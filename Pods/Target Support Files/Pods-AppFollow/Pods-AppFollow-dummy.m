f426f43ea7004e26b48639c03da256fd6f8be80e
Alex Gavrishev <alex.gavrishev@gmail.com>
2018-09-02 14:05:08 +0300

    Integrate crashlytics, upgrade libs

---
+++ Pods/Target Support Files/Pods-AppFollow/Pods-AppFollow-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppFollow : NSObject
+@end
+@implementation PodsDummy_Pods_AppFollow
+@end
--- update.sh
+++ update.sh
@@ -5,5 +5,8 @@ carthage update --platform ios
 rm -rf AppFollow/Firebase/*
 mv -f Carthage/Build/iOS/Firebase*.framework AppFollow/Firebase/
 mv -f Carthage/Build/iOS/nanopb.framework AppFollow/Firebase/
-mv -f Carthage/Build/iOS/GoogleToolboxForMac.framework AppFollow/Firebase/
+mv -f Carthage/Build/iOS/GoogleAppMeasurement.framework AppFollow/Firebase/
+mv -f Carthage/Build/iOS/GoogleUtilities.framework AppFollow/Firebase/
 mv -f Carthage/Build/iOS/Protobuf.framework AppFollow/Firebase/
+mv -f Carthage/Build/iOS/MeasurementNanoPB.framework AppFollow/Firebase/
+
