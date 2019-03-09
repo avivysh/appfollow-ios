a9d9fc69c3e4d17c57dffb297e964b88ddc84523
Alex Gavrishev <alex.gavrishev@gmail.com>
2019-03-09 19:34:16 +0200

    Upgrade libs

--- update.sh
+++ update.sh
@@ -8,4 +8,3 @@ mv -f Carthage/Build/iOS/nanopb.framework AppFollow/Firebase/
 mv -f Carthage/Build/iOS/GoogleAppMeasurement.framework AppFollow/Firebase/
 mv -f Carthage/Build/iOS/GoogleUtilities.framework AppFollow/Firebase/
 mv -f Carthage/Build/iOS/Protobuf.framework AppFollow/Firebase/
-
